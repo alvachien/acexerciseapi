80555e837733cc5c902e63398d118d2a876136b8
Alva Chien <alvachien@163.com>
2017-10-18 21:42:43 +0800

    Continues the development.

---
+++ acexerciseapi/Controllers/ExerItemTypeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace acexerciseapi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ExerItemType")]
+    public class ExerItemTypeController : Controller
+    {
+        // GET: api/ExerItemType
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ExerItemType/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/ExerItemType
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/ExerItemType/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ acexerciseapi/Sqls/DBSchema.sql
@@ -0,0 +1,39 @@
+﻿
+/****** Object:  Table [dbo].[t_exitems]    Script Date: 2017-10-18 9:38:49 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[t_exitems](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Question] [nvarchar](100) NOT NULL,
+	[Answer] [nvarchar](100) NULL,
+	[CreatedBy] [nvarchar](50) NOT NULL,
+	[CreatedAt] [datetime] NULL,
+	[Types] [nvarchar](50) NULL,
+ CONSTRAINT [PK_t_exitems] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[t_extype]    Script Date: 2017-10-18 9:39:28 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[t_extype](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[NAME] [nvarchar](50) NOT NULL,
+	[DETAILS] [nvarchar](100) NULL,
+ CONSTRAINT [PK_t_extype] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
